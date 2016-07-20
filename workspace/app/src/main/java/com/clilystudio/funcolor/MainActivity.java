16ab11232af98b584c0ea4c91efbb0a143b71180
clilystudio <bullsgl@gmail.com>
2016-07-20 14:56:19 +0800

    Version 0.1

--- workspace/app/src/main/java/com/clilystudio/funcolor/MainActivity.java
+++ workspace/app/src/main/java/com/clilystudio/funcolor/MainActivity.java
@@ -1,5 +1,21 @@
+/*
+ * Copyright (C) 2016 ClilyStudio
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.clilystudio.funcolor;
 
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.graphics.Point;
 import android.media.MediaPlayer;
@@ -7,14 +23,12 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.Display;
 import android.view.GestureDetector;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.WindowManager;
 import android.widget.TextView;
 
 import java.lang.ref.WeakReference;
@@ -27,6 +41,12 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
     private static final int DELAY = 1000;
     private static final int PERIOD = 60;
     private static final int ALPHA = 255;
+    private static final int MIN_DISTANCE = 64;
+    private static final int MIN_VELOCITY = 240;
+    private static final int MAX_VELOCITY = 24000;
+    private static final String KEY_HUE = "Hue";
+    private static final String KEY_SATURATION = "Saturation";
+    private static final String KEY_LIGHTNESS = "Lightness";
     BackgroundSound mBackgroundSound;
     private MediaPlayer mMediaPlayer;
     private TimerHandle mHandler = new TimerHandle(this);
@@ -34,7 +54,7 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
     private TimerTask mTimerTask;
     private TextView mContentView;
     private int mHue = 160;
-    private int mSaturation = 0;
+    private int mSaturation = 80;
     private int mLightness = 0;
     private Point mSize = new Point();
     private GestureDetector mGestureDetector;
@@ -43,8 +63,8 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        loadPrefs();
         getWindowManager().getDefaultDisplay().getSize(mSize);
-        Log.e(TAG, "width=" + mSize.x + " height=" + mSize.y);
         setContentView(R.layout.activity_main);
         mContentView = (TextView) findViewById(R.id.tv_title);
         if (mContentView != null) {
@@ -79,10 +99,63 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
 
             @Override
             public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-                Log.e(TAG, "velocityX=" + velocityX + " velocityY=" + velocityY );
+                float distanceX = Math.abs(e1.getX() - e2.getX());
+                float distanceY = Math.abs(e1.getY() - e2.getY());
+                if (distanceX > MIN_DISTANCE) {
+                    if (Math.abs(velocityX) > MIN_VELOCITY) {
+                        distanceX *= (1.0f + Math.abs(velocityX) / MAX_VELOCITY);
+                    }
+                }
+                if (distanceY > MIN_DISTANCE) {
+                    if (Math.abs(velocityY) > MIN_VELOCITY) {
+                        distanceY *= (1.0f + Math.abs(velocityY) / MAX_VELOCITY);
+                    }
+                }
+                int mOffsetX = (int) (12.0f * distanceX / mSize.x);
+                if (velocityX > 0) {
+                    mHue += mOffsetX;
+                } else {
+                    mHue -= mOffsetX;
+                }
+                if (mHue < 0) {
+                    mHue += 240;
+                }
+                if (mHue >= 240) {
+                    mHue = mHue % 240;
+                }
+
+                int mOffsetY = (int) (24.0f * distanceY / mSize.y);
+                if (velocityY > 0) {
+                    mSaturation += mOffsetY;
+                } else {
+                    mSaturation -= mOffsetY;
+                }
+                if (mSaturation < 0) {
+                    mSaturation += 480;
+                }
+                if (mSaturation >= 480) {
+                    mSaturation = mSaturation % 480;
+                }
+                setContentColor();
                 return false;
             }
         });
+    }
+
+    private void loadPrefs() {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        mHue = prefs.getInt(KEY_HUE, 160);
+        mSaturation = prefs.getInt(KEY_SATURATION, 160);
+        mLightness = prefs.getInt(KEY_LIGHTNESS, 160);
+    }
+
+    private void savePrefs() {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putInt(KEY_HUE, mHue);
+        editor.putInt(KEY_SATURATION, mSaturation);
+        editor.putInt(KEY_LIGHTNESS, mLightness);
+        editor.apply();
     }
 
     @Override
@@ -95,6 +168,7 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
     @Override
     protected void onStop() {
         super.onStop();
+        savePrefs();
         mBackgroundSound.cancel(true);
         if (mMediaPlayer != null) {
             mMediaPlayer.pause();
